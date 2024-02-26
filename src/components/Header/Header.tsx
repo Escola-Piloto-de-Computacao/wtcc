b908d3bd0fa1f9dd4d8df02e5b59452c0cbdb8d6
PedroFBV <pedrofigueiraboaviagem@gmail.com>
2024-02-26 00:11:02 -0300

    Line under selected option in header
    A animated line now displays to the user which part of the website he is on

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LogoWTCC from '../../assets/LogoeventLogo.png'
 
-// TODO linha que segue o usuário em baixo de cada opção do menu
-
 const Header = () => {
+    const [activeSelection, setActiveSelection] = useState('Home');
+
+    const handleSelection = (e: React.MouseEvent<HTMLAnchorElement>, selection: string) => {
+        e.preventDefault();
+        setActiveSelection(selection);
+    }
+
     return (
         <header className="lg:flex lg:justify-center pt-8">
             <div className="lg:flex items-center justify-between fixed bg-gray-50 max-w-screen-xl mx-auto z-20 h-24 border-gray-200 px-8 lg:px-16 rounded-lg" >
-                <div className="flex items-center ml-6 mr-14 py-3 w-24">
+                <div className="flex items-center ml-6 mr-14 py-3 w-24"> {/* add href to Home */}
                     <img src={LogoWTCC} alt="Logo IX WTCC" className="w-3/4 hidden lg:block" />
                 </div>
                 <div className="flex gap-20 font-semibold text-xl lg:mr-20 lg:justify-start lg:items-start justify-center items-center">
-                    <a href="" className="hover:text-gray-500 hidden lg:block">Home</a>
-                    <a href="" className="hover:text-gray-500 hidden lg:block">Eventos</a>
-                    <a href="" className="hover:text-gray-500 hidden lg:block">Galeria</a>
-                    <a href="" className="hover:text-gray-500 hidden lg:block">Cronograma</a>
-                    <a href="" className="hover:text-gray-500 hidden lg:block">FAQ</a>
+                    <a href="" className={`hover:text-gray-500 hidden lg:block underline-transition ${activeSelection === 'Home' ? 'active' : ''}`} onClick={(e) => handleSelection(e, 'Home')}>Home</a>
+                    <a href="" className={`hover:text-gray-500 hidden lg:block underline-transition ${activeSelection === 'Eventos' ? 'active' : ''}`} onClick={(e) => handleSelection(e, 'Eventos')}>Eventos</a>
+                    <a href="" className={`hover:text-gray-500 hidden lg:block underline-transition ${activeSelection === 'Galeria' ? 'active' : ''}`} onClick={(e) => handleSelection(e, 'Galeria')}>Galeria</a>
+                    <a href="" className={`hover:text-gray-500 hidden lg:block underline-transition ${activeSelection === 'Cronograma' ? 'active' : ''}`} onClick={(e) => handleSelection(e, 'Cronograma')}>Cronograma</a>
+                    <a href="" className={`hover:text-gray-500 hidden lg:block underline-transition ${activeSelection === 'FAQ' ? 'active' : ''}`} onClick={(e) => handleSelection(e, 'FAQ')}>FAQ</a>
                 </div>
             </div>
-        </header>
+        </header >
     );
 }
 
