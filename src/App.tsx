259444451376132d7712efcf31921eddac01a339
PedroFBV <pedrofigueiraboaviagem@gmail.com>
2024-02-25 23:50:54 -0300

    Events component added
    Starting the development of the Events component which will allow users to pick which event to see more about. Also added some dependencies.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import Events from './components/Events/Events';
 import Footer from './components/Footer/Footer';
 
 function App() {
     return (
         <div className="App">
             <Header />
+            {/* <Home /> */}
+            <Events />
             <Footer />
         </div>
     );
