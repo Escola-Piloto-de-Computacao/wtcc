0c0cff2984a5bbd2369e3fcb6340a42d74ed0c9a
PedroFBV <pedrofigueiraboaviagem@gmail.com>
2024-02-26 09:35:36 -0300

    Creation of the Sponsorship component
    Sponsorship component created but still needs some changes and improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import Sponsorship from './components/Sponsorship/Sponsorship';
 import Footer from './components/Footer/Footer';
 
 function App() {
     return (
         <div className="App">
             <Header />
+            <Footer />
+            <Sponsorship />
+            <Footer />
+            <Footer />
+            <Footer />
             <Footer />
         </div>
     );
