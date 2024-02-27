7d5e7ccfbc294bb3846cbad4a58439226fb2828b
PedroFBV <pedrofigueiraboaviagem@gmail.com>
2024-02-27 10:55:20 -0300

    Paragraph and spacing fixes
    Changed the text to test word-breaks, increased the spacing between elements and added stylistic elements to paragraph text

--- src/components/Scientificshow/Scientificshow.tsx
+++ src/components/Scientificshow/Scientificshow.tsx
@@ -5,7 +5,7 @@ const mostra = [
     {
         img: teste1,
         legenda: "legenda",
-        descricao: "uma foto muito grande e legalsdjfglk asbdfloasublfubspfu dhlsiudfoiwuhf iuheouhhebf dus ufepfu hgpsiuef hpushefp u silufhoasiue fhpisohfasu"
+        descricao: "O VIII Workshop Técnico-Científico de Computação do RN visa despertar e estimular o interesse dos estudantes de graduação e de Pós-Graduação em Computação na pesquisa científica sobre temas tanto para estudantes quanto para profissionais de computação."
     },
     {
         img: teste1,
@@ -32,16 +32,14 @@ const mostra = [
 
 const Botao = () => {
     return (
-    <div className="flex justify-center text-4xl">
-        <p>
-            Submeta seu artigo! <br />
-            <br />
-            {/* Adicionar link */}
-            <a className="font-bold text-white bg-green-700 rounded-md" href="https://www.google.com.br/?hl=pt-BR">
-                Inscreva-se
-            </a>
-        </p>
-    </div>
+        <div className="flex justify-center text-4xl">
+            <p>
+                Submeta seu artigo! <br />
+                <br />
+                {/* Adicionar link */}
+                <a className="font-bold text-white bg-green-700 rounded-md" href="https://www.google.com.br/?hl=pt-BR">Inscreva-se</a>
+            </p>
+        </div>
     )
 }
 
@@ -49,20 +47,19 @@ const Scientific_show = () => {
     return (
         <div>
             <div className="flex justify-evenly flex-wrap m-20 gap-4">
-            {
-                mostra.map((m, index) => {
-                    return (
-                        <div className="w-1/4" key={index}>
-                            <div>
-                                <img src={m.img} alt={m.descricao} />
-                                <p className="text-left text-sm text-slate-500 ml-6"> {m.legenda} </p>
+                {
+                    mostra.map((m, index) => {
+                        return (
+                            <div className="w-1/4 mb-12" key={index}>
+                                <div className="mb-4">
+                                    <img src={m.img} alt={m.descricao} />
+                                    <p className="text-left text-sm text-slate-500 ml-6"> {m.legenda} </p>
+                                </div>
+                                <p className="text-justify indent-4 text-balance hyphens-auto break-words"> {m.descricao} </p>
                             </div>
-
-                            <p> {m.descricao} </p>
-                        </div>
-                    )
-                })
-            }
+                        )
+                    })
+                }
             </div>
             <Botao />
         </div>
