2dfda82858088e9ee2c4c19afc9b1d2c5e69ac0d
PedroFBV <pedrofigueiraboaviagem@gmail.com>
2024-02-27 11:08:49 -0300

    Handled case of empty array, fixed button design
    Added interface for Mostra array object type to allow for the site to have no Mostras available and fixed the button for submitting Mostras.

--- src/components/Scientificshow/Scientificshow.tsx
+++ src/components/Scientificshow/Scientificshow.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import teste1 from "../../assets/Scientific_show/teste1.svg";
 
-const mostra = [
+interface MostraItem {
+    img: string;
+    descricao: string;
+    legenda: string;
+    // add other properties here
+}
+
+const mostra: MostraItem[] = [/*
     {
         img: teste1,
         legenda: "legenda",
@@ -27,18 +34,17 @@ const mostra = [
         legenda: "legenda5",
         descricao: "uma foto muito grande e meio legal"
     }
-
+*/
 ];
 
 const Botao = () => {
     return (
-        <div className="flex justify-center text-4xl">
-            <p>
-                Submeta seu artigo! <br />
-                <br />
-                {/* Adicionar link */}
-                <a className="font-bold text-white bg-green-700 rounded-md" href="https://www.google.com.br/?hl=pt-BR">Inscreva-se</a>
-            </p>
+        <div className="flex flex-col col-span-1 justify-center text-4xl">
+            <p className="mb-7">Submeta seu artigo!</p>
+            {/* Adicionar link */}
+            <div>
+                <a className="font-bold text-white bg-green-700 rounded-md px-3 py-1" href="https://www.google.com.br/?hl=pt-BR">Clique aqui!</a>
+            </div>
         </div>
     )
 }
@@ -48,17 +54,21 @@ const Scientific_show = () => {
         <div>
             <div className="flex justify-evenly flex-wrap m-20 gap-4">
                 {
-                    mostra.map((m, index) => {
-                        return (
-                            <div className="w-1/4 mb-12" key={index}>
-                                <div className="mb-4">
-                                    <img src={m.img} alt={m.descricao} />
-                                    <p className="text-left text-sm text-slate-500 ml-6"> {m.legenda} </p>
+                    mostra.length === 0 ? (
+                        <p>Sem mostras ainda!</p>
+                    ) : (
+                        mostra.map((m, index) => {
+                            return (
+                                <div className="w-1/4 mb-12" key={index}>
+                                    <div className="mb-4">
+                                        <img src={m.img} alt={m.descricao} />
+                                        <p className="text-left text-sm text-slate-500 ml-6"> {m.legenda} </p>
+                                    </div>
+                                    <p className="text-justify indent-4 text-balance hyphens-auto break-words"> {m.descricao} </p>
                                 </div>
-                                <p className="text-justify indent-4 text-balance hyphens-auto break-words"> {m.descricao} </p>
-                            </div>
-                        )
-                    })
+                            )
+                        })
+                    )
                 }
             </div>
             <Botao />
