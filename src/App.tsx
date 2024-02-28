e5b0591ef1cdc2306f251d3424eb931177a567d0
Clara <claralet1346@gmail.com>
2024-02-27 21:50:20 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+
 
 function App() {
     return (
         <div className="App">
-            <Header />
+           
             
         </div>
     );
