a6b1a8b9f03ca3b1e8dc34837ab175a1544d2dcb
PedroFBV <pedrofigueiraboaviagem@gmail.com>
2024-03-01 17:43:55 -0300

    Component done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
-
+import Header from './components/Header/Header';
+import Lectures from './components/Lectures/Lectures';
+import Footer from './components/Footer/Footer';
 
 function App() {
     return (
         <div className="App">
-           
-            
+            <Header />
+            <Footer />
+            <Footer />
+            <Lectures />
+            <Footer />
+            <Footer />
         </div>
     );
 }
