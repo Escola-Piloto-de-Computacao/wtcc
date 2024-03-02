13fade0992fe725188e9bfc82411ca307d9c9c55
PedroFBV <pedrofigueiraboaviagem@gmail.com>
2024-03-01 22:30:23 -0300

    Component semi-done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import Pictures from './components/Pictures/Pictures';
 import Footer from './components/Footer/Footer';
 
 function App() {
     return (
         <div className="App">
             <Header />
+            <Footer />
+            <Footer />
+            <Pictures />
+            <Footer />
             <Footer />
         </div>
     );
