f77628f22d8f2c3bc30fd355f3b3961b403aa7d3
vinicius-dantasso <vinicius.dantas777@outlook.com>
2024-03-26 10:18:12 -0300

    fixed: sponsors padronized

--- src/components/Sponsorship/index.tsx
+++ src/components/Sponsorship/index.tsx
@@ -3,7 +3,7 @@ import ccen from '../../assets/sponsors/ccen.png';
 import proec from '../../assets/sponsors/proec.png';
 import ufersa from '../../assets/sponsors/ufersa.png';
 import prograd from '../../assets/sponsors/prograd.png';
-import compLicen from '../../assets/sponsors/compLicen.jpg';
+import compLicen from '../../assets/sponsors/compLicen.png';
 
 //TODO padronize all images to square size
 
@@ -47,22 +47,22 @@ const Circle = () => {
 
 const Sponsorship = () => {
     return (
-        <div className="mt-52">
-            <div className="relative pt-36 flex justify-center pb-32">
-                <Circle />
-                <h1 className="text-4xl">PATROCINADORES</h1>
-            </div>
-            <div className="flex flex-wrap justify-center mx-52 gap-14">
-                {sponsors.map((sponsor, index) => {
-                    return (
-                        <div className="bg-slate-300 w-1/4 p-12 rounded-md m-4" key={index}>
-                            <img src={sponsor.imagem} alt={sponsor.alt} className="w-full" />
-                        </div>
-                    );
-                })}
-            </div>
+      <div className="mt-52">
+        <div className="relative pt-36 flex justify-center pb-32">
+          <Circle />
+          <h1 className="text-4xl">PATROCINADORES</h1>
         </div>
+        <div className="flex flex-wrap justify-center mx-52 gap-6">
+          {sponsors.map((sponsor, index) => {
+            return (
+              <div className="bg-slate-300 w-60 h-60 p-12 rounded-md m-4 flex items-center justify-center" key={index}>
+                <img src={sponsor.imagem} alt={sponsor.alt} className="max-w-full max-h-full h-auto"/>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     );
-};
+  };
 
 export default Sponsorship;
