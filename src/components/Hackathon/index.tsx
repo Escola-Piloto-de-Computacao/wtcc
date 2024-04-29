a16c4b2a8d98ed0a712b42fd5f3eecd479701f51
joas-barros <joas.barros14@hotmail.com>
2024-04-28 22:06:44 -0300

    fixing hackathon layout

--- src/components/Hackathon/index.tsx
+++ src/components/Hackathon/index.tsx
@@ -1,50 +1,57 @@
 import React from "react";
 import { Image } from "antd";
-import { AntDesignOutlined } from "@ant-design/icons";
-import { Avatar } from "antd";
+
+import hackathonPics from "../../assets/hackathon/hackathon.png";
+
+interface hackathonEvent {
+    title: string;
+    description: string;
+    date: string;
+}
+
+const eventCard = (event: hackathonEvent) => {
+    return (
+        <div>
+            <h2 className="mt-7 text-2xl font-bold text-left font-poppings">{event.title}</h2>
+            <p className="mt-2 text-justify">{event.description}</p>
+            <h2 className="mt-2 text-2xl font-bold text-center font-poppings">{event.date}</h2>
+        </div>
+    )
+}
 
 const Hackathon = () => {
-    const selectedSpeaker = {
-        name: "Ainda não há palestrante cadastrado",
-        description: "Ainda não há palestrante cadastrado",
-        img: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-    };
-
-    const lecture = {
-        name: "Ainda não há palestra cadastrada",
-        description: "Ainda não há palestra cadastrada",
-        speaker: selectedSpeaker,
-        img: "https://i.imgur.com/2d8Kc1c.png"
-    };
+    const hackathonEvents : hackathonEvent[] = [
+        {
+        title: "Lançamento do tema",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus porta massa purus, ac cursus ante cursus ut.",
+        date: "data"
+        },
+        {
+        title: "Construção das ideias",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus porta massa purus, ac cursus ante cursus ut.",
+        date: "data"
+        },
+        {
+        title: "Apresentação final",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus porta massa purus, ac cursus ante cursus ut.",
+        date: "data"
+        }
+    ]
 
     return (
-        <div className="w-full grid grid-cols-12 gap-10 mt-10">
-            <div className="col-start-2 col-span-5 rounded-xl">
+        <div className="w-full grid grid-cols-12 gap-10 mt-10 ">
+            <div className=" col-start-2 col-span-5 rounded-xl flex flex-col justify-center">
                 <Image
                     width="100%"
-                    src={lecture.img}
+                    src={hackathonPics}
                     alt="Palestra do VIII WTCC"
                     style={{ borderRadius: "15px" }}
                 />
             </div>
-            <div className="col-start-7 col-span-5 mt-4 ml-6 flex flex-row row-span-1 flex-wrap">
-                <div className="">
-                    <h2 className="text-3xl font-bold text-left font-poppings">{lecture.name}</h2>
-                    <p className="mt-5 ml-6 text-justify">{lecture.description}</p>
-                </div>
-                <div>
-                    <h3 className="text-2xl font-bold text-left font-poppings">{lecture.speaker.name}</h3>
-                    <div className="h-10 mt-5 ml-6 grid grid-cols-6 gap-2">
-                        <div className="mt-1 mr-2">
-                            <Avatar
-                                size={64}
-                                icon={<AntDesignOutlined />}
-                            >
-                                <img src={lecture.speaker.img} alt={"Foto de " + lecture.speaker.name} />
-                            </Avatar>
-                        </div>
-                        <p className="text-justify col-span-5">{lecture.speaker.description}</p>
-                    </div>
+            <div className="col-start-8 col-span-3 mt-4 ml-6 flex flex-col flex-wrap">
+                <h1 className="ml-6 text-4xl font-bold text-center font-poppings">Cronograma</h1>
+                <div className="mt-1 ml-6">
+                    {hackathonEvents.map((event) => eventCard(event))}
                 </div>
             </div>
         </div>
